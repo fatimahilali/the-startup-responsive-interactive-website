90e374ffe444f801eb1b3be0a9d53e9a6235a365
Fatima el Hilali <hilalif@ad.hva.nl>
2025-01-15 22:10:07 +0100

    Add footer code and change location functionality

---
+++ script/footer.js
@@ -0,0 +1,8 @@
+// JavaScript code to handle the location dropdown toggle
+function toggleLocationMenu() {
+    const dropdown = document.querySelector('.location-menu');
+    const isVisible = dropdown.style.display === 'block';
+    dropdown.style.display = isVisible ? 'none' : 'block';
+}
+
+
